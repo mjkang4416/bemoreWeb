19638499ec649b1e716365d0639f709882c29f78
seoyeonii <chaesy02@gmail.com>
2024-02-12 15:07:31 +0900

    배경 html 수정, mypage 앱 생성

--- BACK/urls.py
+++ BACK/urls.py
@@ -17,13 +17,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 import pageapp
-
+from mypage import views
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     #joinFormapp의 urls를 추가했다
     path('', include('joinFormapp.urls')),
     #pageapp 의 urls 추가
-    path('', include('pageapp.urls'))
+    path('', include('pageapp.urls')),
+    #mypage의 urls 추가
+    path('', include('mypage.urls')),
 ]
 
