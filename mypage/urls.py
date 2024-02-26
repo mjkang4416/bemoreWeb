61720c74891ea61bef93893a7c7e19c42f3a0cff
seoyeonii <chaesy02@gmail.com>
2024-02-26 11:51:12 +0900

    풀 받기 전 커밋

--- mypage/urls.py
+++ mypage/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 app_name='mypage'
 
 urlpatterns = [
     path('<str:username>/',views.mypage, name='mypage'),
     path('<str:username>/mypage_edit/',views.mypage_edit, name='mypage_edit'),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
