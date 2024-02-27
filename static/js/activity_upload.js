7f57abe96d47ba7d929ab97d394837f597888ed7
s0obang <tnqls40168@gmail.com>
2024-02-28 02:00:29 +0900

    중간커밋

--- static/js/activity_upload.js
+++ static/js/activity_upload.js
@@ -1,6 +1,14 @@
-// activity_upload.js
-
 $(document).ready(function(){
+    // 파일이 선택될 때마다 실행되는 함수
+    $("#activityImage").change(function() {
+        // 선택된 파일들의 이름을 가져와서 <div id="selectedPhotoName"></div>에 추가
+        var selectedPhotoNames = "";
+        for (var i = 0; i < this.files.length; i++) {
+            selectedPhotoNames += '<p class="selectedPhotoName">' + this.files[i].name + "</p>";
+        }
+        $("#selectedPhotoName").html(selectedPhotoNames);
+    });
+
     // "사람 추가" 버튼 클릭 시 모달 열기
     $("#choicePeople").click(function(){
         $("#myModal").css("display", "block");
@@ -15,23 +23,21 @@ $(document).ready(function(){
     $(".profile-item").click(function() {
         var profileId = $(this).data("profile-id");
         var profileName = $(this).text();
-        var profilePictureUrl = $(this).data("profile-photo-url"); // 프로필 사진 URL 가져오기
+        var profilePictureUrl = $(this).data("profile-photo-url");
 
         // 동적으로 profileSet 추가
         var newProfileSet = $('<div class="profileSet"></div>');
-        newProfileSet.append('<div><img src="' + profilePictureUrl + '" alt="Profile Picture" class="profile-image"></div>');
-        newProfileSet.append('<div><p>' + profileName + '</p></div>');
-        newProfileSet.append('<input type="hidden" name="user_id" value="">' + profileName + '</p></div>');
+        newProfileSet.append('<img src="' + profilePictureUrl + '" alt="Profile Picture" class="profile-image">');
+        newProfileSet.append('<input type="hidden" name="user_id" value="'+ profileName.trim() +'">');
+        newProfileSet.append('<input type="text" name="user_say" id="user_say" class="sayinput">');
 
         // 선택한 프로필을 선택 목록에 추가
         $("#selectedUsers").append(newProfileSet);
 
-        // 선택한 프로필 정보를 선택 목록에 추가
-        $("#selectedProfiles").append('<input type="hidden" name="selected_profiles[]" value="' + profileId + '">');
-        var newUserInput = $('<div class="userinput"></div>');
-        newUserInput.append('<label for="user_say_' + profileId + '">사용자의 말:</label>');
-        newUserInput.append('<input type="text" name="user_say_' + profileId + '" id="user_say_' + profileId + '">');
+        
 
-        $("#profileSet").append(newUserInput);
+        // addZone의 높이를 동적으로 늘리기
+        var addZoneHeight = $("#addZone").height();
+        $("#addZone").height(addZoneHeight + 30);
     });
 });
