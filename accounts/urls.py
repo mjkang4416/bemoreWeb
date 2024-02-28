047e687686831bc068cd7e66e054d70652291dd8
mjkang4416 <mjkang4416@naver.com>
2024-02-28 14:19:42 +0900

    css적용전 로그인완료

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,12 +1,11 @@
+from django import views
 from django.urls import  path
-from django.contrib.auth.views import LoginView, LogoutView
-from .views import *
+from django.contrib.auth import views as auth_views
 from . import views
 
 app_name = 'accounts'
 
 urlpatterns = [
-
-path('login/', LoginView.as_view(template_name="accounts/login.html"),name='login'),
-path('logout/', LogoutView.as_view(), name='logout'), 
+path('login/', auth_views.LoginView.as_view(template_name="accounts/login.html"),name='login'),
+path('logout/', views.logout_view, name='logout'), 
 ]
