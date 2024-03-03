6f53665f135bd4c813f926c899763e35569e91cf
s0obang <109025445+s0obang@users.noreply.github.com>
2024-03-03 13:03:48 +0900

    Update settings.py

--- BACK/settings.py
+++ BACK/settings.py
@@ -28,7 +28,8 @@ SECRET_KEY = 'django-insecure-vty4*+4*_c!@%8s+v(ic2)@p^ky$zagzldsz@dm6!3qupi2%7^
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    '13.124.93.231'
+    '13.124.93.231',
+    'ec2-13-124-93-231.ap-northeast-2.compute.amazonaws.com'
 ]
 
 
